e833de9bf7facc6829bed2f339c2c58ef0b1843c
Adam Zaninovich <adam.zaninovich@gmail.com>
2018-05-24 10:55:25 -0700

    initial commit

---
+++ config/config.exs
@@ -0,0 +1,13 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+use Mix.Config
+
+config :alice,
+  api_key: System.get_env("SLACK_API_TOKEN"),
+  state_backend: :redis,
+  redis: System.get_env("REDIS_URL")
+
+config :alice_google_images,
+  cse_id: System.get_env("GOOGLE_CSE_ID"),
+  cse_token: System.get_env("GOOGLE_CSE_TOKEN"),
+  safe_search_level: :high
---
+++ mix.exs
@@ -0,0 +1,36 @@
+defmodule AliceMD.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :alice_md,
+      version: "0.1.0",
+      elixir: "~> 1.6",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      mod: {
+        Alice, %{
+          handlers: [
+            Alice.Handlers.DJO,
+            Alice.Handlers.GoogleImages
+          ]
+        }
+      }
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:alice, "~> 0.3.6"},
+      {:alice_google_images, "~> 0.1.4"},
+      {:alice_karma, "~> 0.3.5"}
+    ]
+  end
+end
